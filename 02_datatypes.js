371033871787b6987b4223a28e7cdbeb951c4934
Shriyog <pundeshriyog2004@gmail.com>
2025-02-16 06:57:15 +0000

    datatypes

---
+++ 02_datatypes.js
@@ -0,0 +1,17 @@
+// alert(3 + 3)     WORKS IN BROWSER NOT IN NODE
+ let name = "Shriyog"
+ let age = 20
+ let isLoggedIn = true
+
+ /*
+ number 
+ bigint
+ boolean
+ string => ""
+ null => represent empty value
+ undefined => not defined
+ symbol => unique
+
+ object
+ */
+console.log(typeof "Shriyog");
