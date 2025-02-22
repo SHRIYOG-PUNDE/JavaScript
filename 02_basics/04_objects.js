f88d7a69220b49144c89dcbd132b6f1a34e3b143
Shriyog <pundeshriyog2004@gmail.com>
2025-02-22 12:06:37 +0000

    final objects

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -28,6 +28,42 @@ const obj4 = {5 : "E", 6 : "F"}
 // const obj3 = Object.assign({}, obj1, obj2, obj4) //first empty bracket is target and other are source 
 // // if empty bracket is not given obj1 would be considered as target
 
-const obj3 = {...obj1, ...obj2, ...obj4}
-console.log(obj3);
+// const obj3 = {...obj1, ...obj2, ...obj4}
+// console.log(obj3);
+
+const user = [  // array of onjects
+    {
+        id : 1,
+        email : "shri@hotmail.con"
+    },
+    {
+        id : 2,
+        email : "me@hotmail.con"
+    }
+]
+console.log(user[1].email);
+
+console.log(tinderUser);
+console.log(Object.keys(tinderUser));  // gives all keys of tinderUser object
+console.log(Object.values(tinderUser));  // gives all values of tinderUser object
+console.log(Object.entries(tinderUser));  // gives all key - values of tinderUser object in seperate arry inside array
+
+console.log(tinderUser.hasOwnProperty("name"));
+
+
+const course = {
+    courseName : "Shri ml courses",
+    price : 1000,
+    instructor : "Master Shri"
+}
+
+
+console.log(course.instructor);
+const {instructor} = course
+console.log(instructor);
+
+
+
+
+
 
