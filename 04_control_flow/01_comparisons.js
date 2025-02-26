1a106e5ab68787dcf199c48c5a01057cb350a41e
Shriyog <pundeshriyog2004@gmail.com>
2025-02-26 13:51:37 +0000

    final

--- 04_control_flow/01_comparisons.js
+++ 04_control_flow/01_comparisons.js
@@ -14,9 +14,36 @@ if( 2 != 3)
 {
     console.log("True");
 }
-const score = 200
+const score = 500
 if(score > 100)
 {
     const power = "fly" 
     console.log(`users powers ${power}`);
+}
+
+if(score < 500) console.log(`score is ${score}`);
+
+if(score < 150)
+{
+    console.log("Less then 150");
+}
+else if (score < 200) {
+    console.log("Less then 200");
+}
+else{
+    console.log("Grater then 200");
+}
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = true
+const loggedInFromEmail = true
+
+if(userLoggedIn && debitCard)
+{
+    console.log("Allowed to buy course");
+}
+
+if (loggedInFromEmail || loggedInFromGoogle) {
+    console.log("User is logged in");
 }
