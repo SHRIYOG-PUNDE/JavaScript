6bce3e5f4e13bfd8265430860d0df956846685f9
Shriyog <pundeshriyog2004@gmail.com>
2025-02-26 14:26:37 +0000

    switch

---
+++ 04_control_flow/02_switch.js
@@ -0,0 +1,18 @@
+const month = 3
+switch (month) {
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("February");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    case 4:
+        console.log("April");
+        break;
+    default:
+        console.log("wrong match");
+        break;
+} // if no break statment used the executes all cases after the true one except default
