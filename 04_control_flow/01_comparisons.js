a51eaf4ad9a07124370fb9abc1b2e94cdff4e065
Shriyog <pundeshriyog2004@gmail.com>
2025-02-26 13:40:03 +0000

    comparisons

---
+++ 04_control_flow/01_comparisons.js
@@ -0,0 +1,22 @@
+const isUserLoggedIn = true
+const tempreature = 41
+
+if(tempreature == "41")  // true
+{
+    console.log("True"); 
+}
+if(tempreature === "41")  // false
+{
+    console.log("True");
+}
+
+if( 2 != 3)
+{
+    console.log("True");
+}
+const score = 200
+if(score > 100)
+{
+    const power = "fly" 
+    console.log(`users powers ${power}`);
+}
