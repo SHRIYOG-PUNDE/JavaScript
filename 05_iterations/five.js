266daaa5912d9a85c031cf8eb39f3fcc4a4a4e65
Shriyog <pundeshriyog2004@gmail.com>
2025-02-27 08:35:22 +0000

    forEach

---
+++ 05_iterations/five.js
@@ -0,0 +1,24 @@
+const coading = ["java", "Python", "Cpp", "Ruby", "JavaScript"]
+// coading.forEach( function (item) {
+//     console.log(item);
+// }  )
+
+// coading.forEach( (item) => {
+//     console.log(item);
+// })
+
+// function printMe(item)
+// {
+//     console.log(item);   
+// }
+// coading.forEach(printMe)
+
+
+// coading.forEach( (item, index, arr) => {
+//     console.log(item, index, arr);
+// })
+
+const myCoading = {
+    languageName: "JavaScript",
+    languuageFile : "js"
+}
