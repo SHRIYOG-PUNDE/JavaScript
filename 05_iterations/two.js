fa53ff15dca7c78856229eb0ef6a2ac2ee4b0fb0
Shriyog <pundeshriyog2004@gmail.com>
2025-02-27 06:33:12 +0000

    while loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,15 @@
+// while loop
+
+// let myArray = ["bulbul", "lambda", "choty", "pistuly"]
+// let i = 0;
+// while (i < myArray.length ) {
+//     console.log(myArray[i]);
+//     i = i + 1
+// }
+
+// do while loop
+let score = 1
+do {
+    console.log(`Score is : ${score} `);
+    score++    
+} while (score <= 10);
