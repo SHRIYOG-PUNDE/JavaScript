c833da19a52c859ee24a8aee9eca2700a755389d
Shriyog <pundeshriyog2004@gmail.com>
2025-02-27 06:33:35 +0000

    for of and map

---
+++ 05_iterations/three.js
@@ -0,0 +1,31 @@
+// let myArray = ["bulbul", "lambda", "choty", "pistuly"]
+
+// for(const ele of myArray)
+// {
+//     console.log(myArray[ele]);
+// }
+
+// const greetings = "Hello World !"
+// for(const greet of greetings)
+// {
+//     console.log(`Each character is : ${greet}`);   
+// }
+
+
+// MAPS
+
+const map = new Map()
+map.set('IN', "india")
+map.set("US", "united stated")
+map.set("FR", "france")
+map.set('IN', "india")  // repetation is not allowed
+
+// for (const ele of map) {
+//     console.log(ele);   // gives array  [ 'IN', 'india' ]
+// }
+
+
+// for (const [key, val] of map) {
+//     console.log(`${key} :- ${val}`);   // key is stroed using "key" and value using "val" 
+// }
+
