94d71f27bd88d589f0431b35e53a82da1d656bc8
Shriyog <pundeshriyog2004@gmail.com>
2025-03-10 17:02:31 +0000

    dom

--- 06_DOM/browserConsolQueries.js
+++ 06_DOM/browserConsolQueries.js
@@ -1,1 +1,23 @@
-document.getElementById("area")
+document.getElementById("basson1")  // this will return h1 tag
+
+document.getElementById("basson1").getAttribute("name")  //this will return lemon 
+
+document.getElementById("basson1").setAttribute("class, gendaful")  // this will set attribute gendaful as class and it always overwrites the previous content 
+
+const title = document.getElementById("basson1")
+title.style.backgroundColor = "red"
+
+title.textContent  // this will return html content no matter if its hidden by css properties
+
+title.innerText // this will show only display content
+
+title.innerHTML  // this will give content of tag if u write more html inside it show as it is
+
+document.getElementsByClassName("lemons")
+document.querySelector("h2")
+document.querySelectorAll("h2")
+document.querySelector("#basson1")
+document.querySelector(".gendafullo")
+
+const turnGreen = ulvar.querySelector("li")
+turnGreen.style.backgroundColor = "green"
